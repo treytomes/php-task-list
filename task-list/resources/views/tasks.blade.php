6251fd58c0f91b6f209f7508a74a4c2d8cfdef0a
Trey Tomes <trey.tomes@gmail.com>
2020-09-07 08:27:52 -0500

    Implement task delete.

--- task-list/resources/views/tasks.blade.php
+++ task-list/resources/views/tasks.blade.php
@@ -60,7 +60,14 @@
                                 </td>
 
                                 <td>
-                                    <!-- TODO: Delete Button -->
+                                    <form action="{{ url('task/'.$task->id) }}" method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+
+                                        <button type="submit" class="btn btn-danger">
+                                            <i class="fa fa-trash"></i> Delete
+                                        </button>
+                                    </form>
                                 </td>
                             </tr>
                         @endforeach
--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -58,5 +58,7 @@ Route::post('/task', function (Request $request) {
  * Delete Task
  */
 Route::delete('/task/{task}', function (Task $task) {
-    //
+    $task->delete();
+
+    return redirect('/');
 });
