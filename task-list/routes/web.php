f21cd923c7db9a673685e95414a2e1fd480df23c
Trey Tomes <trey.tomes@gmail.com>
2020-09-07 12:48:58 -0500

    Move task routes into a controller.

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -21,49 +21,16 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
 
-/**
- * Show Task Dashboard
- */
 Route::get('/', function () {
-    $tasks = Task::orderBy('created_at', 'asc')->get();
-
-    return view('tasks', [
-        'tasks' => $tasks
-    ]);
-});
-
-/**
- * Add New Task
- */
-Route::post('/task', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-
-    return redirect('/');
-});
-
-/**
- * Delete Task
- */
-Route::delete('/task/{task}', function (Task $task) {
-    $task->delete();
-
-    return redirect('/');
+    return redirect('/tasks');
 });
 
 // Is this the same as Route::auth()?
 Auth::routes();
 
+// TODO: I may end up removing this one.
 Route::get('/home', 'HomeController@index')->name('home');
